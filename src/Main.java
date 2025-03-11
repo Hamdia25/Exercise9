7d4328e268e22bc602820c8ff2cb5da0244df528
Hamdia25 <hamdiaden033@gmail.com>
2025-03-11 13:32:00 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    Phone call = new Phone("Hamdi", "Hamdiaden033@gmail.com", "123-344-3424");
+    Phone call = new Phone("Yasemin", "Yasemin74@gmail.com", "123-344-3424");
     Email email = new Email("James", "James636@gmail.com");
     email.contact();
     call.contact();
